c60c85aec0bb73824a74306bb252faf77c4a32c8
Lokendra <lokilokendra12345@gmail.com>
2020-11-30 01:12:34 +0530

    [lokendra] refactored a code to uc_5

--- EmployeePayrollService/Program.cs
+++ EmployeePayrollService/Program.cs
@@ -7,24 +7,13 @@ namespace EmployeePayrollService
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to employee payroll");
-            EmployeeRepo repo = new EmployeeRepo();
+            EmployeeRepo repo = new EmployeeRepo();           
 
-            //UC2 get all the employee Details
+            // repo.UpdateEmployeeSalaryUsingStoredProcedure("Karan", 43540);
 
-            string query = @"select * from Employee_payroll";
+            //Get the employees who joined after certain date range
+            string query = @"select* from Employee_payroll where start_Date between CAST('2020-01-01' as date) and GETDATE()";
             repo.GetAllEmployee(query);
-
-            //Insert some record into employee table
-
-            EmployeePayroll employee = new EmployeePayroll();
-            employee.name = "Rahul";
-            employee.startDate = Convert.ToDateTime("2020-10-01");
-            employee.gender = 'M';
-            employee.Address = "Karnataka";
-            employee.phoneNumber = "+91 9866528888";
-
-
-            repo.UpdateEmployeeSalaryUsingStoredProcedure("Karan", 43540);
 
 
 

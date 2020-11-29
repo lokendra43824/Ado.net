75769751a8a08093e07ba8034f7c218da016bb0d
Lokendra <lokilokendra12345@gmail.com>
2020-11-30 01:18:45 +0530

    [lokendra] refactored a code to uc_7

--- EmployeePayrollService/Program.cs
+++ EmployeePayrollService/Program.cs
@@ -8,6 +8,13 @@ namespace EmployeePayrollService
         {
             Console.WriteLine("welcome to employee payroll");
             EmployeeRepo repo = new EmployeeRepo();
+
+            EmployeePayroll employee = new EmployeePayroll();
+            employee.name = "Rahul";
+            employee.startDate = Convert.ToDateTime("2020-10-01");
+            employee.gender = 'M';
+            employee.Address = "Karnataka";
+            employee.phoneNumber = "+91 9866528888";
             repo.addEmployee(employee);
         }
     }

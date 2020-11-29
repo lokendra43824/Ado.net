0d2b436ef4dc1f53384706332d10d60b818c8dd9
Lokendra <lokilokendra12345@gmail.com>
2020-11-30 01:00:05 +0530

    [lokendra] refactored a code to uc_5

--- EmployeePayrollService/Program.cs
+++ EmployeePayrollService/Program.cs
@@ -9,9 +9,23 @@ namespace EmployeePayrollService
             Console.WriteLine("welcome to employee payroll");
             EmployeeRepo repo = new EmployeeRepo();
 
-            
+            //UC2 get all the employee Details
 
-            repo.UpdateEmployeeAddressUsingStoredProcedure("lokendra", "43540");
+            string query = @"select * from Employee_payroll";
+            repo.GetAllEmployee(query);
+
+            //Insert some record into employee table
+
+            EmployeePayroll employee = new EmployeePayroll();
+            employee.name = "Rahul";
+            employee.startDate = Convert.ToDateTime("2020-10-01");
+            employee.gender = 'M';
+            employee.Address = "Karnataka";
+            employee.phoneNumber = "+91 9866528888";
+
+
+
+
 
 
         }
